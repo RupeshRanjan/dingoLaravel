c097e7315284fa1abc87fa4c7679dc05dcb2c60f
Rupesh Ranjan <rupeshranjanmti@gmail.com>
2019-10-11 08:45:12 +0530

    Changes in Base controller and user controller

--- app/Api/V1/Controllers/UserController.php
+++ app/Api/V1/Controllers/UserController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Api\V1\Controllers\BaseController;
 
-class UserController extends Controller
+class UserController extends BaseController
 {
 	public function index()
 	{
-		return "rupesh";
+		$user = User::all();
+		return $this->response->array($user->toArray());
 	}
 
 	public function show($id)
